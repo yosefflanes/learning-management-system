9f3d48bd4e88ea17be4161d2288c324edd8a8fea
yosefflanes <yosefflanes@gmail.com>
2026-07-13 20:20:41 +0700

    fix: use relative base path for cross-platform compatibility

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/learning-management-system/',
+  base: './learning-management-system/',
 })
