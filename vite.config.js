622ef6481d68b9e173807521a0151fe51dfcd2e3
yosefflanes <yosefflanes@gmail.com>
2026-07-13 20:39:42 +0700

    fix: explicit dynamic base path for multiple hosting

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './learning-management-system/',
+  base: process.env.GITHUB_ACTIONS === 'true' 
+    ? '/learning-management-system/' 
+    : '/',
 })
